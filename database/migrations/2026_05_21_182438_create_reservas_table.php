97225faef5fc079eecbeda858bdc736be1ef75fb
Nicolas Veloso <VelosoN97@users.noreply.github.com>
2026-05-21 14:31:43 -0400

    Commit 20 - Ejecución de la migración de reservas

--- database/migrations/2026_05_21_182438_create_reservas_table.php
+++ database/migrations/2026_05_21_182438_create_reservas_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('reservas', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('servicio_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('horario_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->enum('estado', [
+                'Pendiente',
+                'Confirmada',
+                'Cancelada',
+                'Completada'
+            ])->default('Pendiente');
+
             $table->timestamps();
         });
     }
